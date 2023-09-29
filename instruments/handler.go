08a79740fb2c8556ecf1a66605f5ed60c6d0f234
Hesham <hesham.moneer@outlook.com>
2023-09-29 14:35:00 +0300

    changed handler to be a runner not a wrapper

--- instruments/handler.go
+++ instruments/handler.go
@@ -1,16 +1,12 @@
 package instruments
 
-func HandleErrors(job Job, log Logger, identifier int) Job {
-	wrappedJob := func() {
-		defer func() {
-			if err := recover(); err != nil {
-				log.Error(err)
-				log.Warn("Exited job", identifier)
-			}
-		}()
+func HandleErrors(job Job, log Logger, identifier int) {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Error(err)
+			log.Warn("Exited job", identifier)
+		}
+	}()
 
-		job()
-	}
-
-	return wrappedJob
+	job()
 }
