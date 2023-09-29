69151a7c9ccd3cd9170bedc6e7fa2ae6f1da1bd5
Hesham <hesham.moneer@outlook.com>
2023-09-29 14:36:15 +0300

    Added instruments to cron scheduler

--- scheduler/cron.go
+++ scheduler/cron.go
@@ -11,29 +11,36 @@ import (
 type cron struct {
 	wg   sync.WaitGroup
 	jobs map[int]inst.Job
+	log  inst.Logger
 }
 
-func Cron() cron {
+func Cron(file ...string) cron {
 	return cron{
 		wg:   sync.WaitGroup{},
 		jobs: map[int]inst.Job{},
+		log:  inst.NewLogger(file...),
 	}
 }
 
 func (c *cron) AddFunction(expectedDuration time.Duration, period time.Duration, job inst.Job, identifier int) {
 	c.wg.Add(1)
-	f := func() {
+	loop := func() {
 		for {
-			job()
-			time.Sleep(period)
+			c.log.Info("Started Job", identifier)
+			startTime, endTime := inst.JobTime(job)
+			actualDuration := endTime.Sub(startTime)
+			timeToSleep := period - actualDuration
+
+			time.Sleep(timeToSleep)
+			c.log.Info("Finished Job", identifier, "Expected Duration:", expectedDuration, "Actual Duration:", actualDuration)
 		}
 	}
-	c.jobs[identifier] = f
+	c.jobs[identifier] = loop
 }
 
 func (c *cron) RunAll() {
-	for _, job := range c.jobs {
-		go job()
+	for identifier, job := range c.jobs {
+		go inst.HandleErrors(job, c.log, identifier)
 	}
 
 	fmt.Scanln()
